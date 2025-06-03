82ce12d9f2839e8443d168e6f037cc55790574cc
denosaur98 <denosaur98@yandex.ru>
2025-06-03 15:51:55 +0300

    add page transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxt/image'],
-  css: ['public/assets/styles/global.scss']
+  css: ['public/assets/styles/global.scss'],
+  app: {
+     pageTransition: {
+      name: 'page',
+      mode: 'out-in',
+      duration: 500
+    },
+  }
 })
